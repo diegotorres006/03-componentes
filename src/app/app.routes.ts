d012c0e6475f6d64117e763cbcee156ff3a11687
diegotorres006 <jtorresm14@est.ups.edu.ec>
2025-11-21 17:23:19 -0500

    Proyecto Angular 03-componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { EstilosPage } from './features/estilos-page/estilos-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'estilos', component: EstilosPage },
+
+    { path: '', redirectTo: '/estilos', pathMatch: 'full' }
+];
---
+++ src/app/features/estilos-page/estilos-page.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estilos-page',
+  standalone: true,
+  templateUrl: './estilos-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EstilosPage {}
