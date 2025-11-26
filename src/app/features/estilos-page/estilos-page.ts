d0b93bcd3094fab34f155f92787dc28e011c01ae
diegotorres006 <jtorresm14@est.ups.edu.ec>
2025-11-25 22:19:10 -0500

    Cambios: descripción corta

--- src/app/features/estilos-page/estilos-page.ts
+++ src/app/features/estilos-page/estilos-page.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SignalBoxComponent } from "../components/signal-box-component/signal-box-component";
-import { Progressbar } from "../progressbar/progressbar";
+
 
 @Component({
   selector: 'app-estilos-page',
-  imports: [SignalBoxComponent, Progressbar],
+  imports: [SignalBoxComponent],
   standalone: true,
   templateUrl: './estilos-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
